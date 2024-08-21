7135338615902ef2ab7a2a117001e64e1c504367
Sarwar Nazrul <sarwar.nazrul242@gmail.com>
2024-08-21 11:32:17 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,77 @@
+import * as THREE from 'three';
+import './style.css'
+import gsap from 'gsap';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+
+// Create a sphere geometry
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({ color: "#00ff83" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+
+
+// Add a point light
+const light = new THREE.PointLight(0xffffff, 50, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+// Setup camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100);
+camera.position.z = 20;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+// Set renderer size
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
+renderer.render(scene, camera);
+
+ // Orbit controls
+ const controls = new OrbitControls(camera, canvas);
+ controls.enableDamping = true;
+ controls.enablePan = false;
+ controls.enableZoom = false;
+ controls.autoRotate = true;
+ controls.autoRotateSpeed = 5;
+
+
+
+
+
+//Resize
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  // renderer.render(scene, camera);
+});
+
+const loop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+}
+loop();
+
+// Timeline 
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+tl.fromTo(mesh.scale, { z:0, x:0, y:0 }, { z:1, x:1, y:1 })
